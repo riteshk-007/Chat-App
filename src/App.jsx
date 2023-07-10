4125c00b30570ba7e21ed4fb6124097cf6fd3d62
Ritesh <codeshorts007@gmail.com>
2023-07-10 17:34:59 +0530

    setup routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="w-screen h-screen bg-blue-300 flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Register.jsx
@@ -0,0 +1,5 @@
+function Register() {
+  return <div>register</div>;
+}
+
+export default Register;
